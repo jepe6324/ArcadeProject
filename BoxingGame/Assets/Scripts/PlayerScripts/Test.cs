93675c6f012cedcdbea00a96e0c84d5c21a28d9e
Jesper Persson <jepe6324@student.uu.se>
2019-04-11 12:17:53 +0200

    Implemented the new Input api for Unity Unity Version 2018.3.8f1 Input system version 0.2.1 preview
    I don't know if git will implement it for you automatically. But if it Doesn't I'll walk you through it.
    Some changes in Test.cs to test the features of this new input method.

--- BoxingGame/Assets/Scripts/PlayerScripts/Test.cs
+++ BoxingGame/Assets/Scripts/PlayerScripts/Test.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Input;
 
 public class Test : MonoBehaviour
 {
-	public FloatReference testFloat;
+	[SerializeField] private InputAction up;
+	[SerializeField] private InputAction down;
+	[SerializeField] private InputAction right;
+	[SerializeField] private InputAction left;
+
+
+	public FloatReference moveSpeed;
 
 	void Start()
 	{
@@ -15,8 +22,38 @@ public class Test : MonoBehaviour
 	{
 		Vector3 position = this.transform.position;
 
+		if (horizontal != 0)
+			this.transform.Translate(new Vector3(	moveSpeed.value * Time.deltaTime * horizontal,
+													gameObject.transform.position.y));
+	}
 
-		this.transform.Translate(new Vector3(	testFloat.value * Time.deltaTime,
-												gameObject.transform.position.y));
+	private float vertical
+	{
+		get
+		{
+			var keyboard = Keyboard.current;
+			var vertical = 0;
+
+			if (keyboard.wKey.isPressed)
+				vertical = 1;
+			else if (keyboard.sKey.isPressed)
+				vertical = -1;
+			return vertical;
+		}
+	}
+
+	private float horizontal
+	{
+		get
+		{
+			var keyboard = Keyboard.current;
+			var horizontal = 0;
+
+			if (keyboard.dKey.isPressed)
+				horizontal = 1;
+			else if (keyboard.aKey.isPressed)
+				horizontal = -1;
+			return horizontal;
+		}
 	}
 }
