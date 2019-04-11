98f531de97752789ecf321e9f94f253f4f4a04b5
Jesper Persson <jepe6324@student.uu.se>
2019-04-11 11:39:46 +0200

    Initial Commit Contains:	The unity project | Currently a 2D project for simplicitys sake| 			FloatReference.cs 			FloatVariable.cs 			Test.cs 			PlayerMoveSpeed.asset

---
+++ BoxingGame/Assets/Scripts/FloatReference.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FloatReference
+{
+	public bool useConstant = false;
+	public float constantValue;
+	public FloatVariable variable;
+
+	public float value
+	{
+		get
+		{
+			return useConstant ? constantValue :
+								  variable.value;
+		}
+	}
+}
---
+++ BoxingGame/Assets/Scripts/PlayerScripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	public FloatReference testFloat;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		Vector3 position = this.transform.position;
+
+
+		this.transform.Translate(new Vector3(	testFloat.value * Time.deltaTime,
+												gameObject.transform.position.y));
+	}
+}
