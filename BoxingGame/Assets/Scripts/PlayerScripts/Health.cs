648eaebb1d114f12fa86b475eaf961a9ca308077
Malek Sharabi <m.malek1996@hotamail.com>
2019-05-06 11:48:18 +0200

    added a health script

---
+++ BoxingGame/Assets/Scripts/PlayerScripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	public float maxHealth = 100.0f;
+	public float currentHealth;
+
+	void Start()
+	{
+		currentHealth = maxHealth;
+	}
+		
+
+	public void ReduceHealth(int damage)
+	{
+		currentHealth -= damage;
+
+		Debug.Log(currentHealth);
+	}
+		
+   
+	void Update ()
+	{
+		if (currentHealth <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
