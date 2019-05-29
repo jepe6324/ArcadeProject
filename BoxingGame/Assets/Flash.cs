1e1ebe85920960043adb4856e7aeb7bfd773d2ee
Mihail Angello Motso <anxhelo91moco@gmail.com>
2019-05-29 12:02:43 +0200

    cool shit

---
+++ BoxingGame/Assets/Flash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Flash : MonoBehaviour
+{
+    public float onsec;
+    public float Switch;
+    public Text Punch;
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            FadeIn();
+            yield return new WaitForSeconds(onsec);
+            FadeOut();
+            yield return new WaitForSeconds(onsec);
+        }
+    }
+    void FadeIn()
+    {
+        Punch.CrossFadeAlpha(0.2f, Switch, false);
+    }
+    void FadeOut()
+    {
+        Punch.CrossFadeAlpha(1.0f, Switch, false);
+    }
+}
