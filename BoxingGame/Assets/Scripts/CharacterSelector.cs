14a750d84fe264389fae55fe53f6fdb84048760b
Jesper Persson <jepe6324@student.uu.se>
2019-06-03 11:05:56 +0200

    CharacterSelector.cs

---
+++ BoxingGame/Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector
+{
+	public static string player1Character;
+	public static string player2Character;
+}
