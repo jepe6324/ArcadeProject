3ec7c7366eae126d4cf6da25b0ac7654ca761575
Jesper Persson <jepe6324@student.uu.se>
2019-06-05 11:17:21 +0200

    Final version of the game?

---
+++ BoxingGame/Assets/Scripts/CharacterSelection/IdleDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IdleDetector : MonoBehaviour
+{
+	public float maxIdle; // How long the game can be idle before we return to splash screen.
+	private float idleTime; // How long the game has currently been idle
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		idleTime = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!Input.anyKey)
+		{
+			idleTime += Time.deltaTime;
+		}
+		else
+		{
+			idleTime = 0;
+		}
+
+		if (idleTime > maxIdle)
+		{
+			AudioManager.StopMusic("Title Music");
+			SceneManager.LoadScene("SplashImageIntro");
+		}
+	}
+}
